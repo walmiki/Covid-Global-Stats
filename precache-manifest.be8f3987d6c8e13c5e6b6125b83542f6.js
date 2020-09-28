e1f18b4ff23a8efa1af95ca597b79288b99e2443
Sumit Walmiki <walmiki240@gmail.com>
2020-09-28 16:35:28 +0530

    Updates

---
+++ precache-manifest.be8f3987d6c8e13c5e6b6125b83542f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca07d8076d8a1f16e4c51fb5864221f",
+    "url": "/Covid-Global-Stats/index.html"
+  },
+  {
+    "revision": "d2d67731db21ea38ad24",
+    "url": "/Covid-Global-Stats/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "83f8b48ecf5e0a4cf44b",
+    "url": "/Covid-Global-Stats/static/css/main.6a5f0b37.chunk.css"
+  },
+  {
+    "revision": "d2d67731db21ea38ad24",
+    "url": "/Covid-Global-Stats/static/js/2.a51e6a37.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/Covid-Global-Stats/static/js/2.a51e6a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f8b48ecf5e0a4cf44b",
+    "url": "/Covid-Global-Stats/static/js/main.5cfd0922.chunk.js"
+  },
+  {
+    "revision": "b56369d9bc8dccca3b26",
+    "url": "/Covid-Global-Stats/static/js/runtime-main.af3b666c.js"
+  }
+]);
